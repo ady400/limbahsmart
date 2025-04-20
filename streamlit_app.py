53abb9dede56d2831fa28ef095a6407a76d267eb
ady400 <raehan223@sma.belajar.id>
2025-04-20 20:22:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,37 @@
-streamlit
-python.pdf
+.navigation ul li a
+
+indicator
+
+{
+
+{
+
+position: absolute;
+
+top: -50%;
+
+width: 70px;
+
+height: 70px;
+
+background: #29fd53;
+
+border-radius: 50%;
+
+border: 6px solid var(--clr);
+
+transition: 0.55;
+
+position: relative;
+
+display: flex;
+
+justify-content: center;
+
+align-items: center;
+
+flex-direction: column;
+
+width: 100%;
+
+text-align: center;
