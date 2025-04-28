6f1c5d3256fbaa24d99bdc686e4deacfd7dee676
ady400 <raehan223@sma.belajar.id>
2025-04-28 10:26:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Limbah Industri", page_icon="♻️", layout="wid
 
 # Sidebar
 with st.sidebar:
-    st.image("https://cdn-icons-png.flaticon.com/512/1866/1866365.png", width=100)
+    st.image("https://www.flaticon.com/free-icon/zero-waste_14119317?term=industrial+waste+processing&page=1&position=1&origin=search&related_id=14119317", width=100)
     st.title("♻️ Limbah Industri")
     st.markdown("**Belajar & Simulasi Limbah Industri** 🌍")
     st.markdown("---")
