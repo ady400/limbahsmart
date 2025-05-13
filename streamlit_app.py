e67ff4345c0599fc76506941c706ee83b4f46748
ady400 <raehan223@sma.belajar.id>
2025-05-13 14:16:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ if menu == "🏠 Beranda":
     st.markdown("""
         <style>
         .hero {
-            background: linear-gradient(135deg, #d4edda, green ligth);
+            background: linear-gradient(135deg, #d4edda, hijau muda);
             color: white;
             padding: 40px 20px;
             border-radius: 20px;
